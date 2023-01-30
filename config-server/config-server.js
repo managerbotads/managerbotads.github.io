aa66c9506e643fa555c8e2c694224ce55cac015a
unknown <adschecklimit@gmail.com>
2023-01-30 18:48:26 +0700

    update

--- config-server/config-server.js
+++ config-server/config-server.js
@@ -1,2 +1,2 @@
-// const server = "https://3ff5-2402-800-6313-6f19-fc66-6ae5-b82f-9e00.ap.ngrok.io";
-const server = "http://localhost";
+const server = "https://3ff5-2402-800-6313-6f19-fc66-6ae5-b82f-9e00.ap.ngrok.io";
+// const server = "http://localhost";
